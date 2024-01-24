f5a10449605355ce080687b41c354044e58de536
CampioniMan <campioni.daniel@gmail.com>
2024-01-24 02:27:25 -0300

    Added Odin color palletes

--- Assets/Scripts/CellView.cs
+++ Assets/Scripts/CellView.cs
@@ -1,9 +1,20 @@
-﻿using TMPro;
+﻿using System;
+using JetBrains.Annotations;
+using Sirenix.OdinInspector;
+using TMPro;
 using UnityEngine;
 
 public class CellView : MonoBehaviour
 {
     [SerializeField] private TMP_Text displayedNumber;
+    [ColorPalette] [OnValueChanged("UpdateDisplayedNumberColor")]
+    [SerializeField] private Color textColor;
+
+    [UsedImplicitly]
+    private void UpdateDisplayedNumberColor()
+    {
+        displayedNumber.color = textColor;
+    }
 
     public void SetState(CellState state)
     {
